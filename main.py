a5c6fdc77c10bb9d6d5483eba0b6483a81452d19
Stefan Hoffmeister <hoffmeister.stefan99@gmail.com>
2021-11-05 17:46:09 +0100

    refactor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from telegram.ext import Updater, CommandHandler
+import configparser
+import traceback
 import logging
 import urllib.request, json
 import io
@@ -16,22 +18,46 @@ def help(update, context):
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+
+def check_share(url):
+    split = url.split('/')
+    if 'share' in split[-1]:
+        split = split[:len(split)-1]
+        new_url = ""
+        for s in split:
+            new_url += s + '/'
+        return new_url
+    else:
+        return url
+
 def media(update, context):
     input_str = str(context.args[0]).lower()
+    input_str = check_share(input_str)
     ijson = input_str + '.json'
-    with urllib.request.urlopen(ijson) as response:
-        data = json.loads(response.read().decode())
-        img_url = data[0]['data']['children'][0]['data']['url']
-        split = img_url.split('.')
-        if split[-1] == 'png' or split[-1] == 'jpg':
-            context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=img_url)
-        elif split[-1] == 'mp4' or split[-1] == 'gif':
-            context.bot.sendVideo(chat_id=update.effective_chat.id, video=img_url)
-        else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Was n das für n Dateityp bitte?!")
-    # Delete message by using chat id: update.effective_chat.id and
-    # message id: update.effective_message.id
-    context.bot.delete_message(chat_id=update.effective_chat.id,message_id=update.effective_message.message_id)
+    req = urllib.request.Request(ijson, headers = {
+                'User-agent': 'reddittomedia'
+                })
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            data = json.loads(response.read().decode())
+            if data[0]['data']['children'][0]['data']['is_video']:
+                img_url = data[0]['data']['children'][0]['data']['media']['reddit_video']['fallback_url']
+                context.bot.sendVideo(chat_id=update.effective_chat.id, video=img_url)
+            else:
+                img_url = data[0]['data']['children'][0]['data']['url']
+
+                split = img_url.split('.') # splits url to find filetype
+
+                if split[-1] == 'png' or split[-1] == 'jpg':
+                    context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=img_url)
+                    print("image\n" + img_url)
+                else:        
+                    context.bot.sendVideo(chat_id=update.effective_chat.id, video=img_url)              
+                    print("video\n"+img_url)
+    except Exception:
+        print(traceback.print_exc())
+        print("server overlaod")
 
 
 
